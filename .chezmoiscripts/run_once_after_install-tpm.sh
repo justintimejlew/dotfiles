8f5627f62486ce15e48a0c490c4badf95d5d05d7
justintimejlew <jblewis281@gmail.com>
2026-08-30 22:27:47 -0400

    Converting my dotfiles from being managed by the setup script to chezmoi

---
+++ .chezmoiscripts/run_once_after_install-tpm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euo pipefail
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+fi
---
+++ scripts/setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/bin/mise trust /workspaces/dotfiles/mise.toml && /usr/local/bin/mise install
