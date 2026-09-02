091511afc303cb4381da95b871ae087c2f229784
justintimejlew <jblewis281@gmail.com>
2026-09-02 01:02:18 +0000

    Added code to detect zsh on terminal to setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -euo pipefail	
+
+if command -v zsh >/dev/null; then
+  sudo chsh -s $(command -v zsh) $USER
+fi
+
 if ! command -v chezmoi >/dev/null; then
 	sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:justintimejlew/dotfiles.git
 fi	
