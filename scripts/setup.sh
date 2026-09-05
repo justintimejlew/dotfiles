a027e185acdc77860fe1647ab4c56f9be5501a12
justintimejlew <jblewis281@gmail.com>
2026-09-05 17:20:45 -0400

    Fixed setup scripts and Dockerfile to speed up devpod creation with less warnings

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,7 +8,7 @@ mise trust /workspaces/dotfiles/dot_config/mise/config.toml && mise install
 # /usr/local/bin/mise trust /workspaces/dotfiles/dot_config/mise/config.toml && /usr/local/bin/mise install
 
 if [ ! -d "$HOME/.local/share/chezmoi" ]; then
-  chezmoi init --apply git@github.com/justintimejlew/dotfiles.git
+  chezmoi init --apply https://github.com/justintimejlew/dotfiles.git
 else
   chezmoi apply
 fi
