92d664b4e913acccd20f3591e40913427b49c771
justintimejlew <jblewis281@gmail.com>
2026-09-05 16:42:52 -0400

    Updated scripts/setup since mise and chezmoi is baked into image

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-/usr/local/bin/mise trust /workspaces/dotfiles/dot_config/mise/config.toml && /usr/local/bin/mise install
+set -euo pipefail
+
+mise trust /workspaces/dotfiles/dot_config/mise/config.toml && mise install
+# /usr/local/bin/mise trust /workspaces/dotfiles/dot_config/mise/config.toml && /usr/local/bin/mise install
+
+if [ ! -d "$HOME/.local/share/chezmoi" ]; then
+  chezmoi init --apply git@github.com/justintimejlew/dotfiles.git
+else
+  chezmoi apply
+fi
