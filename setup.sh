21bc505486a86f9c14854854550d916b12d9fd75
justintimejlew <jblewis281@gmail.com>
2026-09-05 12:00:46 -0400

    Added mise to before_install script

--- setup.sh
+++ setup.sh
@@ -5,10 +5,6 @@ if command -v zsh >/dev/null; then
   sudo chsh -s $(command -v zsh) $USER
 fi
 
-if ! command -v mise >/dev/null 2>&1; then
-    curl https://mise.run | sh
-fi
-
 if ! command -v chezmoi >/dev/null; then
   sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:justintimejlew/dotfiles.git
 fi
