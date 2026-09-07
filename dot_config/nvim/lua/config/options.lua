8280d8a802a8ebcd911cc2296bd6fdd58d835fad
justintimejlew <jblewis281@gmail.com>
2026-09-07 18:23:58 -0400

    Updated lazyvim defaults

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -4,6 +4,8 @@ vim.g.snacks_animate = false
 vim.g.lazyvim_check_order = false
 
 vim.opt.ignorecase = true
+vim.opt.mouse = "a"
+vim.opt.autoindent = true
 
 vim.opt.number = false
 vim.opt.relativenumber = false
